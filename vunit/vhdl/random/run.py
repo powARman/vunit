0ecc873aa1c3f4606a0650b7f7952328ca198af4
Olof Kraigher <olof.kraigher@autoliv.com>
2017-07-12 17:00:06 +0200

    Add data_types (old array_util) and random add_on

--- vunit/vhdl/random/run.py
+++ vunit/vhdl/random/run.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2014-2015, Lars Asplund lars.anders.asplund@gmail.com
+# Copyright (c) 2017, Lars Asplund lars.anders.asplund@gmail.com
 
 from os.path import join, dirname
 from vunit import VUnit
@@ -10,7 +10,7 @@ from vunit import VUnit
 root = dirname(__file__)
 
 ui = VUnit.from_argv()
-lib = ui.add_library("lib")
-ui.add_array_util("lib")
+ui.add_random()
+lib = ui.library("vunit_lib")
 lib.add_source_files(join(root, "test", "*.vhd"))
 ui.main()
